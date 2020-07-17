334729515581517df7fae7e71d039dd68b9543b2
miltoncarlosab <miltoncarlosab@gmail.com>
2020-07-17 20:10:08 -0300

    dica: testando coleções

---
+++ Domain/Curso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Curso
+    {
+        public Curso(string nome, int duracao)
+        {
+            Nome = nome;
+            Duracao = duracao;
+        }
+
+        public string Nome { get; private set; }
+        public int Duracao { get; private set; }
+    }
+}
---
+++ Tests/TestandoColecoes.cs
@@ -0,0 +1,31 @@
+using Domain;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestandoColecoes
+    {
+        [TestMethod("Fluent Assertions - Testando Coleções")]
+        public void CollectionAssert()
+        {
+            //Arrange 
+            var cursosA = new List<Curso>
+            {
+                new Curso(nome: ".Net Core Iniciante", duracao: 4),
+                new Curso(nome: "Testes de Unidade com C#", duracao: 6),
+            };
+
+            var cursosB = new List<Curso>
+            {
+                new Curso(nome: ".Net Core Iniciante", duracao: 4),
+                new Curso(nome: "Testes de Unidade com C#", duracao: 6),
+            };
+
+            //Assert
+            cursosA.Should().BeEquivalentTo(cursosB);
+        }
+    }
+}
